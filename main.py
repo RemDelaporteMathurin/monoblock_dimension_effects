41cfc8fbf3a71a22530b2d2bd2c8d960943ab08e
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-06-13 13:24:04 +0000

    uncommented

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-# from src.model_3d.model import model as model_3D
-from src.model_2d.model import model as model_2D
-
-
 # # 3D
+# from src.model_3d.model import model as model_3D
+
 # model_3D.log_level = 20
 # model_3D.initialise()
 # model_3D.run()
 
 # 2D
-# model_2D.initialise()
-# model_2D.run()
+from src.model_2d.model import model as model_2D
+
+model_2D.initialise()
+model_2D.run()
 
 
 # 1D
